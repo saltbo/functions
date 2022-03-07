8792e7c0138938e47c07a33e5679357fc59e3867
saltbo <saltbo@foxmail.com>
2022-03-07 20:26:21 +0800

    chore: add lambda-go

---
+++ todo-reminder/go.mod
@@ -0,0 +1,10 @@
+module github.com/saltbo/funcs/todo-reminder
+
+go 1.17
+
+require github.com/aws/aws-lambda-go v1.28.0
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // indirect
+	gopkg.in/telebot.v3 v3.0.0 // indirect
+)
--- todo-reminder/main.go
+++ todo-reminder/main.go
@@ -1,3 +1,24 @@
 package main
 
-// 一个提醒事项接口，由外部定时器触发，触发后去发送一个提示/询问
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type MyEvent struct {
+	Name string `json:"name"`
+}
+
+// HandleRequest 一个提醒事项接口，由外部定时器触发，触发后去发送一个提示/询问
+func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
+	fmt.Println(ctx, name)
+
+	return fmt.Sprintf("Hello %s!", name.Name), nil
+}
+
+func main() {
+	// 默认使用Lambda驱动，也可以换成其他的
+	lambda.Start(HandleRequest)
+}
